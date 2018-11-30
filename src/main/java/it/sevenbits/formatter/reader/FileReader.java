669a16c308513b27bc21c6800daea9dc8c0e34dc
DProtsenko <deadbender27@gmail.com>
2018-11-30 17:51:46 +0600

    fixed fileReader (removed ready() check)

--- src/main/java/it/sevenbits/formatter/reader/FileReader.java
+++ src/main/java/it/sevenbits/formatter/reader/FileReader.java
@@ -8,42 +8,49 @@ import java.io.IOException;
 
 /**
  * implementation of IReader to work with file
- * */
+ */
 public class FileReader implements IReader, AutoCloseable {
     private BufferedReader br;
+    private int lastchar;
+
     /**
      * constructor for FileReader
+     *
      * @param path - path to file
      * @throws FormatterException if any ex-s occurs
-     * */
+     */
     public FileReader(final String path) throws FormatterException {
         try {
             br = new BufferedReader(new java.io.FileReader(path));
+            lastchar = br.read();
         } catch (IOException ex) {
             throw new FormatterException(FormatterErrorCode.ERROR_READING_FILE, ex);
         }
     }
+
     /**
      * method for checking the existence of the next element
      *
-     * @throws FormatterException if any ex-s occurs
      * @return condition
-     * */
-    public boolean hasNext() throws FormatterException {
-        try {
-            return br.ready();
-        } catch (IOException ex) {
-            throw new FormatterException(FormatterErrorCode.ERROR_READING_FILE, ex);
-        }
+     */
+    public boolean hasNext() {
+        return lastchar != -1;
     }
+
     /**
      * method to read next char in file
+     *
      * @return readed char
      * @throws FormatterException if any ex-s occurs
-     * */
+     */
     public char read() throws FormatterException {
         try {
-            return (char) br.read();
+            if (lastchar == -1) {
+                throw new IOException();
+            }
+            char toReturn = (char) lastchar;
+            lastchar = br.read();
+            return toReturn;
         } catch (IOException ex) {
             throw new FormatterException(FormatterErrorCode.ERROR_READING_FILE, ex);
         }
